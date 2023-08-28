614c134b3bd59b57617eff04430042fe067b09b1
acerolajuice69 <joao.gui.conrado@gmail.com>
2023-08-28 10:55:22 -0300

    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

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:pet_app_qrcode/screens/login.page.dart';
 
+import 'screens/register.phone.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -16,7 +18,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.purple,
       ),
       debugShowCheckedModeBanner: false,
-      home: const LoginPage(),
+      home: const RegisterPhone(),
     );
   }
 }
