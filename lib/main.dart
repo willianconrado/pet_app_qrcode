1f2d9b9cf66dd4901c9e2faedf4b9c90d4c5d31e
Guilherme Conrado <joao.gui.conrado@gmail.com>
2023-08-29 00:22:35 -0300

    boas novas

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:pet_app_qrcode/screens/address.dart';
 import 'package:pet_app_qrcode/screens/login.page.dart';
-
-import 'screens/address.dart';
 import 'screens/register.phone.dart';
 
 void main() {
--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -1,1 +1,11 @@
 // Aqui ficara a pagina principal do app
+import 'package:flutter/material.dart';
+
+class alo extends StatelessWidget {
+  const alo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
