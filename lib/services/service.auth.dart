c5be6f944fcfa4371379b988ab892b79154c612f
Willian Conrado <willian.conrado@gmail.com>
2023-09-01 08:05:49 -0300

    Verificando validações tela register.screen

--- lib/services/service.auth.dart
+++ lib/services/service.auth.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 
 class AuthService {
-  FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
   registerUser({
     required String email,
     required String password,
