a4d0464ffccbdf4de1b9547824f0889da3b04c4b
Willian Conrado <willian.conrado@gmail.com>
2023-09-10 19:30:09 -0300

    Criando o RefreshIndicator na notifications.screen

--- lib/screens/notifications.screen.dart
+++ lib/screens/notifications.screen.dart
@@ -1,7 +1,20 @@
 import 'package:flutter/material.dart';
 
-class NotificationsPage extends StatelessWidget {
+class NotificationsPage extends StatefulWidget {
   const NotificationsPage({super.key});
+
+  @override
+  _NotificationsPageState createState() => _NotificationsPageState();
+}
+
+class _NotificationsPageState extends State<NotificationsPage> {
+  Future<void> _refreshNotifications() async {
+    // Implementar a lógica das notificaçãoes
+    await Future.delayed(const Duration(seconds: 2));
+    setState(() {
+      // Atualizar a lista de notificações
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -17,38 +30,41 @@ class NotificationsPage extends StatelessWidget {
         backgroundColor: Colors.white.withOpacity(0),
         elevation: 0,
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment
-              .center, // Centralize o conteúdo horizontalmente
-          children: <Widget>[
-            Image.asset("assets/notifications.png"),
-            const SizedBox(
-              height: 16,
-            ),
-            const Text(
-              "Sem novas notificações",
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 25,
+      body: RefreshIndicator(
+        onRefresh: _refreshNotifications,
+        child: ListView(children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment
+                .center, // Centralize o conteúdo horizontalmente
+            children: <Widget>[
+              Image.asset("assets/notifications.png"),
+              const SizedBox(
+                height: 16,
               ),
-            ),
-            const SizedBox(
-              height: 16,
-            ),
-            const Text(
-              "Fique atento! Nós te avisaremos sobre qualquer atualização envolvendo seu pet.",
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                color: Colors.blueGrey,
+              const Text(
+                "Sem novas notificações",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 25,
+                ),
               ),
-            ),
-            const SizedBox(
-              height: 16,
-            ),
-          ],
-        ),
+              const SizedBox(
+                height: 16,
+              ),
+              const Text(
+                "Fique atento! Nós te avisaremos sobre qualquer atualização envolvendo seu pet.",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.blueGrey,
+                ),
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+            ],
+          ),
+        ]),
       ),
     );
   }
